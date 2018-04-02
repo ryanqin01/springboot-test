95e3b41e36e498c1aec6976e326591d69210d523
Ryan Qin <rqin@opentext.com>
2018-04-02 13:00:17 +0800

    feature: add kafka and lombok

--- springboot-test/src/main/java/com/opentext/springboot/MainApp.java
+++ springboot-test/src/main/java/com/opentext/springboot/MainApp.java
@@ -4,14 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
+
+import com.opentext.springboot.message.KafkaSender;
 
 @SpringBootApplication
 public class MainApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MainApp.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(MainApp.class, args);
+		KafkaSender sender = context.getBean(KafkaSender.class);
+		for (int i = 0; i < 3; i++) {
+			// 调用消息发送类中的消息发送方法
+			sender.send();
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Bean
---
+++ springboot-test/src/main/java/com/opentext/springboot/message/Message.java
@@ -0,0 +1,12 @@
+package com.opentext.springboot.message;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Message {
+	private Long id; // id
+	private String msg; // 消息
+	private Date sendTime; // 时间戳
+}
