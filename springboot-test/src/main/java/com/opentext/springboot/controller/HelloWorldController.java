b6e82465a0438217a56187dd73e0023992fdb111
ryanqin01 <ryanqin01@gmail.com>
2018-06-14 17:27:30 +0800

    enhance some codes

--- springboot-test/src/main/java/com/opentext/springboot/controller/HelloWorldController.java
+++ springboot-test/src/main/java/com/opentext/springboot/controller/HelloWorldController.java
@@ -9,16 +9,4 @@ public class HelloWorldController {
 	public String sayHello() {
 		return "Hello Spring Boot! Hello Mybatis!";
 	}
-=======
-package com.opentext.springboot.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HelloWorldController {
-	@RequestMapping("/")
-	public String sayHello() {
-		return "Hello Spring Boot! Hello Mybatis!";
-	}
 }
--- springboot-test/src/main/java/com/opentext/springboot/service/EmployeeService.java
+++ springboot-test/src/main/java/com/opentext/springboot/service/EmployeeService.java
@@ -9,15 +9,3 @@ import com.opentext.springboot.domain.Employee;
 public interface EmployeeService extends SolrCrudRepository<Employee, Integer> {
 	List<Employee> findByusername(String username);
 }
-=======
-package com.opentext.springboot.service;
-
-import java.util.List;
-
-import org.springframework.data.solr.repository.SolrCrudRepository;
-
-import com.opentext.springboot.domain.Employee;
-
-public interface EmployeeService extends SolrCrudRepository<Employee, Integer> {
-	List<Employee> findByusername(String username);
-}
