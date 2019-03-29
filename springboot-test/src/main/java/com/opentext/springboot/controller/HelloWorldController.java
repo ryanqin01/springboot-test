0b1992c9cc493ba91e8ab4044fb46a6f853298af
ryanqin01 <ryanqin01@gmail.com>
2019-03-29 17:36:16 +0800

    add banner, add internationalization, fix bugs

--- springboot-test/src/main/java/com/opentext/springboot/controller/HelloWorldController.java
+++ springboot-test/src/main/java/com/opentext/springboot/controller/HelloWorldController.java
@@ -1,12 +1,21 @@
 package com.opentext.springboot.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-	@RequestMapping("/")
-	public String sayHello() {
-		return "Hello Spring Boot! Hello Mybatis!";
+
+	@Autowired
+	private MessageSource messageSource;
+
+	@GetMapping(path = "/")
+	public String sayHello(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+		return messageSource.getMessage("hello.message", null, locale);
 	}
 }
