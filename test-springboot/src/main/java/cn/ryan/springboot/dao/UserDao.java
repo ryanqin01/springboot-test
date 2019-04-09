5ab197bcfddac162ff178a1b2b92406d09125267
ryanqin01 <ryanqin01@gmail.com>
2019-04-09 17:38:56 +0800

    delete duplicate folder

--- test-springboot/src/main/java/cn/ryan/springboot/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package cn.ryan.springboot.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cn.ryan.springboot.model.User;
-
-public interface UserDao extends JpaRepository<User, Integer> {
-
-}
