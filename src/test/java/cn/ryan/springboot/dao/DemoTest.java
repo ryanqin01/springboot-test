d4bb45db2227b7a4cd8cdcb756ee14c7a3e485fe
ryanqin01 <ryanqin01@gmail.com>
2019-04-10 11:53:25 +0800

    add demo test

---
+++ src/test/java/cn/ryan/springboot/dao/DemoTest.java
@@ -0,0 +1,12 @@
+package cn.ryan.springboot.dao;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DemoTest {
+
+	@Test
+	public void testFieldName() {
+		Assert.assertTrue("wo".equalsIgnoreCase("WO"));
+	}
+}
