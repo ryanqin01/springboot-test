37b869a0ced9502ae09546fdf4699bc542b0443e
ryanqin01 <ryanqin01@gmail.com>
2019-04-17 12:00:14 +0800

    improve zuul

--- test-springboot-assist/src/main/java/cn/ryan/springboot/assist/AssistServiceProxy.java
+++ test-springboot-assist/src/main/java/cn/ryan/springboot/assist/AssistServiceProxy.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import cn.ryan.springboot.model.User;
 
-@FeignClient(name = "test-springboot-main-service")
+// @FeignClient(name = "test-springboot-main-service")
+@FeignClient(name = "test-springboot-gateway-service")
 @RibbonClient(name = "test-springboot-main-service")
 public interface AssistServiceProxy {
 
-	@GetMapping("/users/{id}")
+	// @GetMapping("/users/{id}")
+	@GetMapping("test-springboot-main-service/users/{id}")
 	public User getUserById(@PathVariable String id);
 }
--- test-springboot-naming/src/main/java/org/test/springboot/naming/NamingApplication.java
+++ test-springboot-naming/src/main/java/org/test/springboot/naming/NamingApplication.java
@@ -3,7 +3,9 @@ package org.test.springboot.naming;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
 @EnableEurekaServer
 public class NamingApplication {
